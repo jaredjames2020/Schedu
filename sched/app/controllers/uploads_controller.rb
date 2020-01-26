321217e1f633126847b6f3fc210e1ebbc1edb894
jaredjames2020 <jaredjames2020@gmail.com>
2020-01-26 11:11:09 -0500

    added current users ability to delete upload

--- sched/app/controllers/uploads_controller.rb
+++ sched/app/controllers/uploads_controller.rb
@@ -16,7 +16,7 @@ class UploadsController < ApplicationController
             erb :'uploads/all'
         end
 
- 
+
          #create - make a post request to '/uploads'
          post '/uploads' do
             uploads = Upload.new(entry: params[:upload][":entry"])
@@ -27,21 +27,21 @@ class UploadsController < ApplicationController
                 erb :'uploads/new'
             end
         end
- 
+
      #READ
          #index - make a get request to '/uploads'
          get '/uploads' do
             @uploads = Note.all.reverse
             @events = Event.all.reverse
             erb :'uploads/index'
-        end 
- 
+        end
+
      #DELETE
          #destroy - make a delete request to '/uploads/:id'
          delete '/uploads/:id' do
             uploads = Upload.find(params[:id])
             uploads.destroy
-            redirect '/uploads'
+            redirect '/uploads/all'
         end
- 
+
  end
