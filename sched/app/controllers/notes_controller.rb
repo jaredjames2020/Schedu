cd81d8310ef27da3f41e71e8bde1264279885ef8
jaredjames2020 <jaredjames2020@gmail.com>
2020-01-27 18:48:10 -0500

    note controller protections to edit, patch, delete -- varibles name pluralization reflects qty for convention

--- sched/app/controllers/notes_controller.rb
+++ sched/app/controllers/notes_controller.rb
@@ -12,14 +12,14 @@ class NotesController < ApplicationController
 
         #create - make a post request to '/notes'
         post '/notes' do
-            notes = Note.new(entry: params[:notes][:entry], event_name: params[:notes][:event_name], user_id: current_user.id)
-            events = Event.new(notes_id: notes.id, event_name: notes.event_name, user_id: current_user.id)
-            if notes.save
-                events.save
-                notes.event = events
-                events.notes_id = notes.id
-                notes.save
-                events.save
+            note = Note.new(entry: params[:notes][:entry], event_name: params[:notes][:event_name], user_id: current_user.id)
+            event = Event.new(notes_id: note.id, event_name: note.event_name, user_id: current_user.id)
+            if note.save
+                event.save
+                note.event = event
+                event.notes_id = note.id
+                note.save
+                event.save
             redirect '/notes'
             else
                 erb :'notes/new'
@@ -29,40 +29,56 @@ class NotesController < ApplicationController
     #READ
         #index - make a get request to '/notes'
         get '/notes' do
-            @notes = Note.all.reverse
-            @events = Event.all.reverse
+            @notes = current_user.notes.reverse
+            @events = current_user.events.reverse
             erb :'notes/index'
-        end 
+        end
 
         #show - make a get request to '/notes/:id'
         get '/notes/:id' do
-            @notes = Note.find(params[:id])
-            @events = Event.find_by(id: @notes.event_id)
-            erb :'notes/show'
+            @note = Note.find(params[:id])
+            #@notes.event
+            @event = @note.event
+            if current_user.id == @note.user_id
+              erb :'notes/show'
+            else
+              redirect '/events'
+            end
         end
 
     #UPDATE
         #edit - make a get request to '/notes/:id/edit'
         get '/notes/:id/edit' do
-            @notes = Note.find(params[:id])
-            @events = Event.find(@notes.event_id)
-            erb :'/notes/edit'
+            @note = Note.find(params[:id])
+            @event = @note.event
+            if current_user.id == @note.user_id
+              erb :'/notes/edit'
+            else
+              redirect '/events'
+            end
         end
 
         #update - make a patch request to '/notes/:id'
         patch '/notes/:id' do
-            notes = Note.find(params[:id])
-            notes.user_id = current_user.id
-            notes.update(params[:notes])
-            redirect to '/events/index'
+            note = Note.find(params[:id])
+            if current_user.id == note.user_id
+              note.update(params[:notes])
+              redirect '/events'
+            else
+              redirect '/events'
+          end
         end
 
     #DELETE
         #destroy - make a delete request to '/notes/:id'
         delete '/notes/:id' do
-            notes = Note.find(params[:id])
-            notes.destroy
-            redirect '/notes'
+            note = Note.find(params[:id])
+            if current_user.id == note.user_id
+              note.destroy
+              redirect '/notes'
+            else
+              redirect '/events'
+          end
         end
 
 end
